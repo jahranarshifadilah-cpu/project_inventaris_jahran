f11198e6a77e5f4cd279d8b7692b6c68f60d5a5f
Kaka <kakaviangi58@gmail.com>
2026-02-02 16:46:35 +0700

    perbaikan error

--- database/migrations/2026_01_23_034232_create_peminjaman_table.php
+++ database/migrations/2026_01_23_034232_create_peminjaman_table.php
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('peminjamen');
+        Schema::dropIfExists('peminjaman');
     }
 };
--- database/migrations/2026_01_23_034300_create_detail_peminjaman_table.php
+++ database/migrations/2026_01_23_034300_create_detail_peminjaman_table.php
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('detail_peminjamen');
+        Schema::dropIfExists('detail_peminjaman');
     }
 };
